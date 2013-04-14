9e992376de7712db84246ecb113f381977697fc4
Shaastra <webops@shaastra.org>
2013-04-14 17:31:12 +0530

    reCAPTCHA tested on server
    Registration has been tested with captcha.
    Static files have been set up properly.

--- application_portal/urls.py
+++ application_portal/urls.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,3 +13,8 @@ urlpatterns = patterns('',
     url(r'^$', 'application_portal.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('django.views.static', (r'^static/(?P<path>.*)$'
+                        , 'serve',
+                        {'document_root': settings.STATIC_ROOT,
+                        'show_indexes': True}))
