841cc9480b92abac47a67ce5793101ddaf899ca9
AbdealiJK <abdealikothari@gmail.com>
2014-04-19 09:33:28 +0530

    Fixed small issues in password to make email better looking

--- application_portal/urls.py
+++ application_portal/urls.py
@@ -13,14 +13,14 @@ urlpatterns = patterns('',
     url(r'^$', 'application_portal.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'template_name':'password/reset.html'}, name='password_reset'),
-    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done',{'template_name':'password/reset_done.html'}, name='password_reset_done'),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'template_name':'password/reset.html', 'extra_context':{'site_name':'shaastra.org',}}, name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done',{'template_name':'password/reset_done.html', 'extra_context':{'site_name':'shaastra.org',}}, name='password_reset_done'),
     url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm',
-        {'template_name':'password/reset_new_password.html'},
+        {'template_name':'password/reset_new_password.html', 'extra_context':{'site_name':'shaastra.org',}},
         name='password_reset_confirm'),
 
-    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete',{'template_name':'password/reset_complete.html'}, name='password_reset_complete'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete',{'template_name':'password/reset_complete.html', 'extra_context':{'site_name':'shaastra.org',}}, name='password_reset_complete'),
 
 )
 
